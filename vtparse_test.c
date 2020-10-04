812970d4d314d4ef11f20ea1b2a4b248e595b89e
Casey Duncan <casey.duncan@coinbase.com>
2020-10-04 15:24:50 -0600

    Denser test output

--- vtparse_test.c
+++ vtparse_test.c
@@ -14,20 +14,24 @@ void parser_callback(vtparse_t *parser, vtparse_action_t action, unsigned int ch
 {
     int i;
 
-    printf("Received action %s\n", ACTION_NAMES[action]);
-    if(ch != 0) printf("Char: 0x%02x ('%c')\n", ch, ch);
+    printf("%s ", ACTION_NAMES[action]);
+    if(ch != 0) printf("0x%02x ('%c') ", ch, ch);
     if(parser->num_intermediate_chars > 0)
     {
-        printf("%d Intermediate chars:\n", parser->num_intermediate_chars);
+        printf("%d Intermediate chars: ", parser->num_intermediate_chars);
         for(i = 0; i < parser->num_intermediate_chars; i++)
-            printf("  0x%02x ('%c')\n", parser->intermediate_chars[i],
+            printf("  0x%02x ('%c') ", parser->intermediate_chars[i],
                                         parser->intermediate_chars[i]);
     }
     if(parser->num_params > 0)
     {
-        printf("%d Parameters:\n", parser->num_params);
+        printf("%d Params: ", parser->num_params);
         for(i = 0; i < parser->num_params; i++)
-            printf("\t%d\n", parser->params[i]);
+            if ((i+1) < parser->num_params) {
+                printf("%d, ", parser->params[i]);
+            } else {
+                printf("%d", parser->params[i]);
+            }
     }
 
     printf("\n");
